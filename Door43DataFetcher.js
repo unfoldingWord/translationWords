9ebcc2dab452eed4c54a534cdcfe28e7842188c1
RoyalSix <jay.a.scottjr@biola.edu>
2016-11-05 11:45:51 -0700

    removed tabs

--- Door43DataFetcher.js
+++ Door43DataFetcher.js
@@ -11,42 +11,42 @@ const USFMParser = require('./USFMParse');
 const suppress = true;
 
 class Door43DataFetcher {
-	constructor() {
-		this.bookList = null;
-	}
-	/**
-  * @description - This function finds the ULB text within a book and returns it as
-  * an object
-  * @param {Object} book - book object from getBook() function that is to be parsed
-  */
-	getULBFromBook(book = { chapters: [] }) {
-		let ulbData = { chapters: [] };
-		if (!book.chapters) {
-			console.error("Error: Input object is in incorrect format");
-			return ulbData;
-		}
-		const usfmRegex = new RegExp("=+\\s*ulb:\\s*=+\\s*<usfm>([^<]+)<\\/usfm>", "i");
-		for (let ch of book.chapters) {
-			let chap = { num: -1, verses: [] };
-			for (let v of ch.verses) {
-				let regRes;
-				try {
-					[, regRes] = usfmRegex.exec(v.file);
-				} catch (e) {
-					if (!suppress) {
-						console.warn("ULB Parse Warning: No ULB Data for chapter " + ch.num + " verse " + v.num);
-						console.warn("File may be in incorrect format");
-					}
-					continue;
-				}
-				let parsed = USFMParser(regRes).chapters[0];
-				if (parsed.num != -1) chap.num = parsed.num;
-				chap.verses = chap.verses.concat(parsed.verses);
-			}
-			if (chap.verses.length != 0) ulbData.chapters.push(chap);
-		}
-		return ulbData;
-	}
+    constructor() {
+        this.bookList = null;
+    }
+    /**
+     * @description - This function finds the ULB text within a book and returns it as
+     * an object
+     * @param {Object} book - book object from getBook() function that is to be parsed
+     */
+    getULBFromBook(book = { chapters: [] }) {
+        let ulbData = { chapters: [] };
+        if (!book.chapters) {
+            console.error("Error: Input object is in incorrect format");
+            return ulbData;
+        }
+        const usfmRegex = new RegExp("=+\\s*ulb:\\s*=+\\s*<usfm>([^<]+)<\\/usfm>", "i");
+        for (let ch of book.chapters) {
+            let chap = { num: -1, verses: [] };
+            for (let v of ch.verses) {
+                let regRes;
+                try {
+                    [, regRes] = usfmRegex.exec(v.file);
+                } catch (e) {
+                    if (!suppress) {
+                        console.warn("ULB Parse Warning: No ULB Data for chapter " + ch.num + " verse " + v.num);
+                        console.warn("File may be in incorrect format");
+                    }
+                    continue;
+                }
+                let parsed = USFMParser(regRes).chapters[0];
+                if (parsed.num != -1) chap.num = parsed.num;
+                chap.verses = chap.verses.concat(parsed.verses);
+            }
+            if (chap.verses.length != 0) ulbData.chapters.push(chap);
+        }
+        return ulbData;
+    }
 }
 
 module.exports = Door43DataFetcher;
