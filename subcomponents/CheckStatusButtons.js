2c370c3d288a21f793e06572c6d13a0e949a7bc2
mannycolon <colonmanuel7@gmail.com>
2016-12-08 21:26:29 -0500

    removed retained question and event listeners

--- subcomponents/CheckStatusButtons.js
+++ subcomponents/CheckStatusButtons.js
@@ -11,43 +11,11 @@ class CheckStatusButtons extends React.Component{
 
   constructor() {
     super();
-    this.state = {
-      selectedOption: "",
-    };
     this.setFlagStateFunction = this.setFlagStateFunction.bind(this);
-    this.updateSelectedOption = this.updateSelectedOption.bind(this);
-  }
-
-  componentWillMount() {
-    api.registerEventListener('goToNext', this.updateSelectedOption);
-    api.registerEventListener('goToPrevious', this.updateSelectedOption);
-    api.registerEventListener('changedCheckStatus', this.updateSelectedOption);
-    api.registerEventListener('goToCheck', this.updateSelectedOption);
-  }
-
-  componentWillUnmount() {
-    api.removeEventListener('goToNext', this.updateSelectedOption);
-    api.removeEventListener('goToPrevious', this.updateSelectedOption);
-    api.removeEventListener('changedCheckStatus', this.updateSelectedOption);
-    api.removeEventListener('goToCheck', this.updateSelectedOption);
-
-  }
-
-  updateSelectedOption(){
-    let currentCheck = this.props.getCurrentCheck();
-    if (typeof currentCheck.retained == "string") {
-      this.setState({selectedOption: currentCheck.retained});
-    }
   }
 
   setFlagStateFunction(newCheckStatus){
     this.props.updateCheckStatus(newCheckStatus);
-  }
-
-  handleOptionChange(changeEvent){
-    this.setState({selectedOption: changeEvent.target.value});
-    let currentCheck = this.props.getCurrentCheck();
-    currentCheck.retained = changeEvent.target.value;
   }
 
   render(){
@@ -64,21 +32,6 @@ class CheckStatusButtons extends React.Component{
             function() {_this.setFlagStateFunction('FLAGGED');}}>
             <Glyphicon glyph="flag" /> {FLAGGED}</Button>
         </ButtonGroup>
-        <h4>The figure has been:</h4>
-        <div className="radio-inline">
-          <label>
-          <input type="radio" value="Retained"
-                      checked={this.state.selectedOption === 'Retained'}
-                      onChange={this.handleOptionChange.bind(this)} /> Retained
-          </label>
-        </div>
-        <div className="radio-inline">
-          <label>
-          <input type="radio" value="Replaced"
-                      checked={this.state.selectedOption === 'Replaced'}
-                      onChange={this.handleOptionChange.bind(this)} /> Replaced
-          </label>
-          </div>
       </div>
     );
   }
