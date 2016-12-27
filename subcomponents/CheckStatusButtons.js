3e4a23e5a162270f4de0168510d4c5b2c97fd281
mannycolon <colonmanuel7@gmail.com>
2016-12-27 13:42:31 -0500

    refactored the CheckStatusButtons component so that it uses a more redux approach by depening on props to function instead of retrieving its own data

--- subcomponents/CheckStatusButtons.js
+++ subcomponents/CheckStatusButtons.js
@@ -9,27 +9,17 @@ const CORRECT = "Correct in Context",
 
 class CheckStatusButtons extends React.Component{
 
-  constructor() {
-    super();
-    this.setFlagStateFunction = this.setFlagStateFunction.bind(this);
-  }
-
-  setFlagStateFunction(newCheckStatus){
-    this.props.updateCheckStatus(newCheckStatus);
-  }
-
   render(){
-    var _this = this;
-    let currentCheck = this.props.getCurrentCheck();
+    let currentCheck = this.props.currentCheck;
     let checkStatus = currentCheck.checkStatus;
     return (
       <div>
         <ButtonGroup style={{width:'100%', paddingBottom: "2.5px"}}>
           <Button style={{width:'50%'}} bsStyle="success" className={checkStatus == 'CORRECT' ? 'active':''} onClick={
-            function() {_this.setFlagStateFunction('CORRECT');}}>
+            function() {this.props.updateCheckStatus('CORRECT');}}>
             <Glyphicon glyph="ok" /> {CORRECT}</Button>
           <Button style={{width:'50%'}} bsStyle="danger" className={checkStatus == 'FLAGGED' ? 'active':''} onClick={
-            function() {_this.setFlagStateFunction('FLAGGED');}}>
+            function() {this.props.updateCheckStatus('FLAGGED');}}>
             <Glyphicon glyph="flag" /> {FLAGGED}</Button>
         </ButtonGroup>
       </div>
