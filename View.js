c7956f2fe795db494d491e948689f9a5b2078b11
mannycolon <colonmanuel7@gmail.com>
2017-02-13 17:10:04 -0500

    since the currenCheck property changed from groupname to phrase made necessary changes to account for those changes

--- View.js
+++ View.js
@@ -82,7 +82,7 @@ class View extends React.Component {
                   <div style={{height: "100%", backgroundColor: "#333333", boxSizing: "border-box"}}>
                     <div style={style.currentWord}>
                       <h4 style={{color: "#FFFFFF"}}>Important Words</h4><br />
-                      {'"' + this.props.currentCheck.groupName + '"'}
+                      {'"' + this.props.groups[this.props.currentGroupIndex]['groupName'] + '"'}
                     </div>
                   </div>
                 </Tab>
