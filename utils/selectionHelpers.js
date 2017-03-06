b126ac9a4aa7b07e29761c517612490ff5ba7ef5
Christopher Klapp <christopher@klapp.name>
2017-03-06 17:21:59 -0500

    Addressed bug with second selection highlighting the first, and not letting second occurrence being highlighted

--- utils/selectionHelpers.js
+++ utils/selectionHelpers.js
@@ -1,12 +1,10 @@
 const _ = require('lodash')
 
-module.exports.selectionArray = function(string, selections) {
-  var selectionArray = []
-  var ranges = module.exports.selectionsToRanges(string, selections)
-  selectionArray = module.exports.spliceStringOnRanges(string, ranges)
-  return selectionArray
-}
-
+/**
+ * Splice string into array of ranges, flagging what is selected
+ * @param {array}  ranges - array of ranges [[int,int],...]
+ * @returns {array} - array of objects [obj,...]
+ */
 module.exports.spliceStringOnRanges = function(string, ranges) {
   var selectionArray = [] // response
   // sort ranges - this ensures we build the string correctly and don't miss selections
@@ -17,16 +15,18 @@ module.exports.spliceStringOnRanges = function(string, ranges) {
   var rangeShift = 0
   ranges.forEach(function(rangeObject) {
     var range = rangeObject.range
-    var subString = remainingString.slice(range[0]-rangeShift,range[1]+1-rangeShift)
-    var splitArray = remainingString.split(subString)
-    var beforeSelection = splitArray[0]
+    // save all the text before the selection
+    var beforeSelection = remainingString.slice(0,range[0]-rangeShift)
     // console.log('beforeSelection: ', beforeSelection)
-    // console.log('subString: ', subString)
-    var afterSelection = splitArray.slice(1).join(subString)
+    // save the text in the selection
+    var selection = remainingString.slice(range[0]-rangeShift,range[1]+1-rangeShift)
+    // console.log('subString: ', selection)
+    // save all the text after the selection
+    var afterSelection = remainingString.slice(range[1]-rangeShift+1)
     // console.log('afterSelection: ', afterSelection)
     selectionArray.push({text: beforeSelection, selected: false})
     selectionArray.push({
-                          text: subString,
+                          text: selection,
                           selected: true,
                           occurrence: rangeObject.occurrence,
                           occurrences: rangeObject.occurrences
@@ -35,13 +35,24 @@ module.exports.spliceStringOnRanges = function(string, ranges) {
     remainingString = afterSelection
     // shift the range up to last char of substring (before+sub)
     rangeShift += beforeSelection.length
-    rangeShift += subString.length
+    rangeShift += selection.length
   })
   selectionArray.push({text: remainingString, selected: false})
   // remove empty text from selectionArray
   return selectionArray
 }
+//
+// Use the following lines to test the previous function
+// var string = "01 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 234 567890"
+// var ranges = [ { range: [ 45, 47 ], occurrence: 2, occurrences: 2 } ]
+// console.log(module.exports.spliceStringOnRanges(string, ranges))
 
+/**
+ * This converts ranges to array of selection objects
+ * @param {string} string - text used to get the ranges of
+ * @param {array} selections - array of selections [obj,...]
+ * @returns {array} - array of range objects
+ */
 module.exports.selectionsToRanges = function(string, selections) {
   var ranges = []
     selections.forEach(function(selection) {
@@ -60,6 +71,34 @@ module.exports.selectionsToRanges = function(string, selections) {
     })
   return ranges
 }
+//
+// Use the following lines to test the previous function
+// var string = "01 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 234 567890"
+// var selections = [
+//   { text: '234', occurrence: 2, occurrences: 2 },
+// ]
+// console.log(module.exports.selectionsToRanges(string, selections))
+
+/**
+ * Splice string into array of substrings, flagging what is selected
+ * @param {string} string - text used to get the ranges of
+ * @param {array} selections - array of selections [obj,...]
+ * @returns {array} - array of objects
+ */
+module.exports.selectionArray = function(string, selections) {
+  var selectionArray = []
+  var ranges = module.exports.selectionsToRanges(string, selections)
+  selectionArray = module.exports.spliceStringOnRanges(string, ranges)
+  return selectionArray
+}
+//
+// Use the following lines to test the previous function
+// var string = "01 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 234 567890"
+// var selections = [
+//   { text: '234', occurrence: 2, occurrences: 2 },
+// ]
+// console.log(module.exports.selectionArray(string, selections))
+
 
 /**
  * This abstracts complex handling of ranges such as order, deduping, concatenating, overlaps
@@ -149,6 +188,10 @@ module.exports.optimizeSelections = function(string, selections) {
 //
 // Use the following lines to test the previous function
 // var string = "0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890"
+// var string = "0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890"
+// var selections = [
+//   { text: '234', occurrence: 2, occurrences: 2 },
+// ]
 // var selections = [
 //   { text: 'not found in here', occurrence: 2, occurrences: 2 },
 //   { text: '234', occurrence: 1, occurrences: 2 },
@@ -160,6 +203,9 @@ module.exports.optimizeSelections = function(string, selections) {
 //   { text: 'asdfghjklzxcvbnmtyui0', occurrence: 1, occurrences: 1 }
 // ]
 // console.log(module.exports.optimizeSelections(string, selections))
+
+
+
 
 
 // Use for testing/debugging...
