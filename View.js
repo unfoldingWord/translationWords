3b0edfc24a33bfa62c523875307384008583dad5
mannycolon <colonmanuel7@gmail.com>
2017-03-12 20:04:40 -0400

    Modified the way we were requiring/getting the submodules (ex. ScripturePane, ProposedChanges, CommentBox, TranslationHelps). Now using the redux props being sent down from the ToolsContainer.js which gets it from the LoaderReducer.js. In other words fully replacing api.getModule()

--- View.js
+++ View.js
@@ -2,36 +2,21 @@
  * @description:
  *  This class defines the entire view for TranslationWords tool
  */
-//Api Consts
-const api = window.ModuleApi;
-const React = api.React;
-const RB = api.ReactBootstrap;
-//Bootstrap consts
-const {Row, Col, Tabs, Tab, Glyphicon} = RB;
-//Modules not defined within TranslationWords
-let ScripturePane = null;
-let ProposedChanges = null;
-let CommentBox = null;
-let TranslationHelps = null;
-//Components
-const DragTargetVerseDisplay = require('./components/BareTargetVerseDisplay.js');
-const ClickTargetVerseDisplay = require('./components/TargetVerseDisplay');
-const GatewayVerseDisplay = require('./components/GatewayVerseDisplay.js');
-const CheckInfoCard = require('./components/CheckInfoCard.js');
-const CheckStatusButtons = require('./components/CheckStatusButtons');
-const HelpInfo = require('./components/HelpInfo');
-const style = require('./css/style');
+import React from 'react'
+import {Row, Col, Tabs, Tab, Glyphicon} from 'react-bootstrap'
+import DragTargetVerseDisplay from './components/BareTargetVerseDisplay.js'
+import ClickTargetVerseDisplay from './components/TargetVerseDisplay'
+import GatewayVerseDisplay from './components/GatewayVerseDisplay.js'
+import CheckInfoCard from './components/CheckInfoCard.js'
+import CheckStatusButtons from './components/CheckStatusButtons'
+import HelpInfo from './components/HelpInfo'
+import style from './css/style'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class View extends React.Component {
-  constructor(){
-    super();
-    ScripturePane = api.getModule('ScripturePane');
-    ProposedChanges = api.getModule('ProposedChanges');
-    CommentBox = api.getModule('CommentBox');
-    TranslationHelps = api.getModule('TranslationHelps');
-  }
   render(){
+    //Modules not defined within translationWords
+    const { ScripturePane, ProposedChanges, CommentBox, TranslationHelps } = this.props.modules;
     let TargetVerseDisplay = null;
     if(this.props.dragToSelect){
       TargetVerseDisplay = <DragTargetVerseDisplay
