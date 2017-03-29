00a21c85a3728ff0673539e2a20fcfa70ff879a1
mannycolon <colonmanuel7@gmail.com>
2017-03-29 12:52:54 -0400

    piping down props based on new nested reducer props

--- View.js
+++ View.js
@@ -2,31 +2,36 @@
  * @description:
  *  This class defines the entire view for TranslationWords tool
  */
-import React from 'react'
-import {Row, Col, Tabs, Tab, Glyphicon} from 'react-bootstrap'
-import CheckInfoCard from './components/CheckInfoCard.js'
-import style from './css/style'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import React from 'react';
+import {Row, Col, Tabs, Tab, Glyphicon} from 'react-bootstrap';
+import CheckInfoCard from './components/CheckInfoCard.js';
+import style from './css/style';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class View extends React.Component {
-  render(){
-    //Modules not defined within translationWords
-    const { ScripturePane, VerseCheck, TranslationHelps } = this.props.modules;
+  render() {
+    // Modules not defined within translationWords
+    const {
+      ScripturePane,
+      VerseCheck,
+      TranslationHelps 
+    } = this.props.modules;
     return (
       <MuiThemeProvider>
         <Row className="show-grid" style={{margin: '0px', bottom: "0px",  height: "100%"}}>
           <Col sm={12} md={6} lg={9} style={{height: "100%", padding: '0px'}}>
-            <ScripturePane {...this.props} currentCheck={this.props.currentCheck} />
-            <CheckInfoCard openHelps={this.props.toggleHelps} showHelps={this.props.showHelps} title={this.props.currentCheck.phrase} file={this.props.currentFile}/>
+            <ScripturePane {...this.props} currentCheck={this.props.checkStoreReducer.currentCheck} />
+            <CheckInfoCard openHelps={this.props.toggleHelps} showHelps={this.props.showHelps} title={this.props.checkStoreReducer.currentCheck.phrase} file={this.props.currentFile}/>
             <VerseCheck
+              {...this.props}
               updateCheckStatus={this.props.updateCheckStatus}
               updateCurrentCheck={this.props.updateCurrentCheck}
-              currentCheck={this.props.currentCheck}
+              currentCheck={this.props.checkStoreReducer.currentCheck}
               goToNext={this.props.goToNext}
               goToPrevious={this.props.goToPrevious}
               verse={this.props.targetVerse}
               direction={this.props.direction}
-              bookName={this.props.bookName}
+              bookName={this.props.checkStoreReducer.book}
             />
           </Col>
           <Col sm={12} md={6} lg={3} style={{height: "100%", padding: "0px"}}>
@@ -35,8 +40,8 @@ class View extends React.Component {
                          style={this.props.showHelps ? style.tHelpsOpen : style.tHelpsClosed}
                          onClick={this.props.toggleHelps} />
                 <div style={{display: this.props.showHelps ? "block" : "none", height: "100vh"}}>
-                  <TranslationHelps currentFile={this.props.currentFile}
-                                  online={this.props.online}/>
+                  <TranslationHelps {...this.props} currentFile={this.props.currentFile}
+                                  online={this.props.statusBarReducer.online}/>
                 </div>
             </div>
           </Col>
