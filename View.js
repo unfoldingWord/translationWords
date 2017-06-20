eebda18d1fda52ad1eb706e81ebf764cc12eff17
Christopher Klapp <christopher@klapp.name>
2017-06-19 20:13:44 -0400

    updated to use tH actions/reducers

--- View.js
+++ View.js
@@ -9,15 +9,21 @@ import style from './css/style';
 
 class View extends React.Component {
 
+  componentWillMount() {
+    let articleId = this.props.contextIdReducer.contextId.groupId;
+    this.props.actions.loadResourceArticle('translationWords', articleId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.contextIdReducer !== nextProps.contextIdReducer) {
+      let articleId = nextProps.contextIdReducer.contextId.groupId;
+      nextProps.actions.loadResourceArticle('translationWords', articleId);
+    }
+  }
+
   render() {
     // Modules not defined within translationWords
-    const {
-      ScripturePane,
-      VerseCheck,
-      TranslationHelps
-    } = this.props.modules;
-
-    let {dataList, currentFile} = this.props;
+    const { ScripturePane, VerseCheck, TranslationHelps } = this.props.modules;
 
     // set the scripturePane to empty to handle react/redux when it first renders without required data
     let scripturePane = <div></div>;
@@ -26,30 +32,35 @@ class View extends React.Component {
       scripturePane = <ScripturePane {...this.props} />
     }
 
+    let { translationWords } = this.props.resourcesReducer.translationHelps;
+    let { contextId } = this.props.contextIdReducer;
+    let articleId = contextId.groupId;
+    let currentFile;
+    if (translationWords && translationWords[articleId]) {
+      currentFile = this.props.resourcesReducer.translationHelps.translationWords[articleId];
+    }
+
     return (
-        <div style={{display: 'flex', flex: 'auto'}}>
-          <div style={{flex: '2 1 1000px', display: "flex", flexDirection: "column"}}>
-            {scripturePane}
-            <CheckInfoCard openHelps={this.props.toggleHelps} showHelps={this.props.showHelps} title={this.props.contextIdReducer.contextId.quote} file={this.props.currentFile}/>
-            <VerseCheck
-              {...this.props}
-              goToNext={this.props.goToNext}
-              goToPrevious={this.props.goToPrevious}
+      <div style={{display: 'flex', flex: 'auto'}}>
+        <div style={{flex: '2 1 1000px', display: "flex", flexDirection: "column"}}>
+          {scripturePane}
+          <CheckInfoCard openHelps={this.props.toggleHelps} showHelps={this.props.showHelps} title={this.props.contextIdReducer.contextId.quote} file={this.props.currentFile}/>
+          <VerseCheck {...this.props} />
+        </div>
+        <div style={{flex: this.props.showHelps ? '1 0 375px' : '0 0 30px', display: 'flex', justifyContent: 'flex-end', marginLeft: '-15px'}}>
+          <div style={style.handleIconDiv}>
+              <Glyphicon glyph={this.props.showHelps ? "chevron-right" : "chevron-left"}
+                         style={style.handleIcon}
+                         onClick={this.props.toggleHelps} />
+          </div>
+          <div style={{ display: this.props.showHelps ? "flex" : "none", flex: '1 0 360px' }}>
+            <TranslationHelps
+              currentFile={currentFile}
+              online={this.props.statusBarReducer.online}
             />
           </div>
-          <div style={{flex: this.props.showHelps ? '1 0 375px' : '0 0 30px', display: 'flex', justifyContent: 'flex-end', marginLeft: '-15px'}}>
-            <div style={style.handleIconDiv}>
-                <Glyphicon glyph={this.props.showHelps ? "chevron-right" : "chevron-left"}
-                           style={style.handleIcon}
-                           onClick={this.props.toggleHelps} />
-            </div>
-            <div style={{ display: this.props.showHelps ? "flex" : "none", flex: '1 0 360px' }}>
-                <TranslationHelps currentFile={currentFile}
-                                  dataList={dataList}
-                                  online={this.props.statusBarReducer.online} />
-            </div>
-          </div>
         </div>
+      </div>
     );
   }
 }
