92b4c5b228d1db3ae4403cd6d750372a7d46d98c
mannycolon <colonmanuel7@gmail.com>
2017-08-07 16:25:05 -0400

    removed console logs

--- Container.js
+++ Container.js
@@ -13,9 +13,7 @@ class Container extends React.Component {
 
   componentWillMount() {
     let { ScripturePane } = this.props.settingsReducer.toolsSettings;
-    console.log(ScripturePane)
     if (!ScripturePane) {
-      console.log("hellooo")
       // initializing the ScripturePane settings if not found.
       this.props.actions.setToolSettings("ScripturePane", "currentPaneSettings", ["ulb"]);
     }
