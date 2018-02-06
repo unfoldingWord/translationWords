f1d5751b798020d23d9cca7b5d80012bd4219d9d
Richard Mahn <rmahn@getmealticket.com>
2018-02-06 16:48:02 -0700

    Adds tests for Container

--- __tests__/Container.test.js
+++ __tests__/Container.test.js
@@ -3,25 +3,31 @@ import React from 'react';
 import Container from '../src/Container';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 jest.mock('../src/components/View.js', () => '[View]');
 
-describe('Container Tests', () => {
-  it('Check Container', () => {
-    let props = {
-      settingsReducer: {
-        toolsSettings: {}
-      },
-      contextIdReducer: {
-        contextId: {
-          groupId: 'groupId'
-        }
-      },
-      actions: {
-        setToolSettings: jest.fn(),
-        loadResourceArticle: jest.fn()
+const props = {
+  settingsReducer: {
+    toolsSettings: {
+      ScripturePane: {
+        currentPaneSettings: ['bhp']
       }
-    };
+    }
+  },
+  contextIdReducer: {
+    contextId: {
+      groupId: 'groupId1'
+    }
+  },
+  actions: {
+    setToolSettings: jest.fn(),
+    loadResourceArticle: jest.fn()
+  }
+};
+
+describe('Container Tests', () => {
+  it('Test Container', () => {
     const component = renderer.create(
       <MuiThemeProvider>
         <Container {...props} />
@@ -29,5 +35,28 @@ describe('Container Tests', () => {
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('Test Container.toggleHelps', () => {
+    const container = shallow(<Container {...props} />).instance();
+    expect(container.state.showHelps).toBeTruthy();
+    container.toggleHelps();
+    expect(container.state.showHelps).not.toBeTruthy();
+    container.toggleHelps();
+    expect(container.state.showHelps).toBeTruthy();
+  });
+
+  it('Test Container.componentWillReceiveProps', () => {
+    const wrapper = shallow(<Container {...props} />);
+    expect(wrapper.props().contextIdReducer.contextId.groupId).toEqual(props.contextIdReducer.contextId.groupId);
+    const newGroupId = 'groupId2';
+    wrapper.setProps({
+      contextIdReducer: {
+        contextId: {
+          groupId: newGroupId
+        }
+      }
+    });
+    expect(wrapper.props().contextIdReducer.contextId.groupId).toEqual(newGroupId);
   });
 });
