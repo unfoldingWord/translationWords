6adbb3f07873b35db1316d3ad38b33feae6a0356
RoyalSix <jay.a.scottjr@biola.edu>
2018-05-08 18:38:30 -0700

    added fix for verse check expanding

--- src/Container.js
+++ src/Container.js
@@ -105,18 +105,7 @@ class Container extends React.Component {
             contextIdReducer={{contextId}}
             projectDetailsReducer={{manifest, projectSaveLocation}}
             actions={actions} />
-          <div style={{display:'flex', flexDirection:'column'}}>
-            <ScripturePane
-              titleLabel="Step 1. Read"
-              closeButtonLabel="Close"
-              expandedScripturePaneTitle="Matthew"
-              expandButtonHoverText="Click to show expanded resource panes" />
-            <CheckInfoCard
-              title="save, saves, saved, safe, salvation"
-              phrase='The term "save" refers to keeping someone from experiencing something bad or harmful. To "be safe" means to be protected from harm or danger.'
-              seeMoreLabel="See More"
-              showSeeMoreButton={true}
-              onSeeMoreClick={() => console.log('CheckInfoCard clicked')} />
+          <div style={{display:'flex', flexDirection:'column', width:'100%'}}>
             <VerseCheck />
           </div>
         </div>
