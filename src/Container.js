1758984cd7175049364206c810317ce5d54e42f1
RoyalSix <jay.a.scottjr@biola.edu>
2018-05-10 20:48:44 -0700

    updates to locale and added methods to control verse check

--- src/Container.js
+++ src/Container.js
@@ -4,15 +4,291 @@ import PropTypes from 'prop-types';
 // helpers
 import * as settingsHelper from './helpers/settingsHelper';
 import * as checkAreaHelpers from './helpers/checkAreaHelpers';
+import {optimizeSelections, normalizeString} from './helpers/selectionHelpers';
+import isEqual from 'deep-equal';
+import usfmjs from 'usfm-js';
+
 //ui-kit components
 import {GroupMenu, VerseCheck, ScripturePane, CheckInfoCard} from 'tc-ui-toolkit';
 
 class Container extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    let verseText = usfmjs.removeMarker(this.verseText());
+    const mode = props.selectionsReducer.selections.length > 0 || verseText.length === 0 ? 'default' : 'select';
     this.state = {
-      showHelps: true,
+      mode: mode,
+      comment: undefined,
+      commentChanged: false,
+      verseText: undefined,
+      verseChanged: false,
+      selections: [],
+      tags: [],
+      dialogModalVisibility: false,
+      goToNextOrPrevious: null,
+      showHelps: false
     };
+    this.saveSelection = this.saveSelection.bind(this);
+    this.cancelSelection = this.cancelSelection.bind(this);
+    this.clearSelection = this.clearSelection.bind(this);
+    this.handleSkip = this.handleSkip.bind(this);
+    this.findIfVerseEdited = this.findIfVerseEdited.bind(this);
+    this.verseText = this.verseText.bind(this);
+
+    let _this = this;
+
+    this.tagList = [
+      ["spelling", "Spelling"],
+      ["punctuation", "Punctuation"],
+      ["grammar", "Grammar"],
+      ["meaning", "Meaning"],
+      ["wordChoice", "Word Choice"],
+      ["other", "Other"]
+    ];
+
+    this.actions = {
+      handleGoToNext() {
+        if (!_this.props.loginReducer.loggedInUser) {
+          _this.props.actions.selectModalTab(1, 1, true);
+          _this.props.actions.openAlertDialog("You must be logged in to save progress");
+          return;
+        }
+        props.actions.goToNext();
+      },
+      handleGoToPrevious() {
+        if (!_this.props.loginReducer.loggedInUser) {
+          _this.props.actions.selectModalTab(1, 1, true);
+          _this.props.actions.openAlertDialog("You must be logged in to save progress");
+          return;
+        }
+        props.actions.goToPrevious();
+      },
+      handleOpenDialog(goToNextOrPrevious) {
+        _this.setState({goToNextOrPrevious});
+        _this.setState({dialogModalVisibility: true});
+      },
+      handleCloseDialog() {
+        _this.setState({dialogModalVisibility: false});
+      },
+      skipToNext() {
+        _this.setState({dialogModalVisibility: false});
+        props.actions.goToNext();
+      },
+      skipToPrevious() {
+        _this.setState({dialogModalVisibility: false});
+        props.actions.goToPrevious();
+      },
+      changeSelectionsInLocalState(selections) {
+        _this.setState({selections});
+      },
+      changeMode(mode) {
+        _this.setState({
+          mode: mode,
+          selections: _this.props.selectionsReducer.selections
+        });
+      },
+      handleComment(e) {
+        const comment = e.target.value;
+        _this.setState({
+          comment: comment
+        });
+      },
+      checkComment(e) {
+        const newcomment = e.target.value || "";
+        const oldcomment = _this.props.commentsReducer.text || "";
+        _this.setState({
+          commentChanged: newcomment !== oldcomment
+        });
+      },
+      cancelComment() {
+        _this.setState({
+          mode: 'default',
+          selections: _this.props.selectionsReducer.selections,
+          comment: undefined,
+          commentChanged: false
+        });
+      },
+      saveComment() {
+        if (!_this.props.loginReducer.loggedInUser) {
+          _this.props.actions.selectModalTab(1, 1, true);
+          _this.props.actions.openAlertDialog("You must be logged in to leave a comment", 5);
+          return;
+        }
+        _this.props.actions.addComment(_this.state.comment, _this.props.loginReducer.userdata.username);
+        _this.setState({
+          mode: 'default',
+          selections: _this.props.selectionsReducer.selections,
+          comment: undefined,
+          commentChanged: false
+        });
+      },
+      handleTagsCheckbox(tag) {
+        let newState = _this.state;
+        if (newState.tags === undefined) newState.tags = [];
+        if (!newState.tags.includes(tag)) {
+          newState.tags.push(tag);
+        } else {
+          newState.tags = newState.tags.filter(_tag => _tag !== tag);
+        }
+        _this.setState(newState);
+      },
+      handleEditVerse(e) {
+        const verseText = e.target.value;
+        _this.setState({
+          verseText: verseText
+        });
+      },
+      checkVerse(e) {
+        let {chapter, verse} = _this.props.contextIdReducer.contextId.reference;
+        const newverse = e.target.value || "";
+        const oldverse = _this.props.resourcesReducer.bibles.targetLanguage.targetBible[chapter][verse] || "";
+        if (newverse === oldverse) {
+          _this.setState({
+            verseChanged: false,
+            tags: []
+          });
+        } else {
+          _this.setState({
+            verseChanged: true
+          });
+        }
+      },
+      cancelEditVerse() {
+        _this.setState({
+          mode: 'default',
+          selections: _this.props.selectionsReducer.selections,
+          verseText: undefined,
+          verseChanged: false,
+          tags: []
+        });
+      },
+      saveEditVerse() {
+        let {loginReducer, actions, contextIdReducer, resourcesReducer} = _this.props;
+        let {chapter, verse} = contextIdReducer.contextId.reference;
+        let before = resourcesReducer.bibles.targetLanguage.targetBible[chapter][verse];
+        let username = loginReducer.userdata.username;
+        // verseText state is undefined if no changes are made in the text box.
+        if (!loginReducer.loggedInUser) {
+          _this.props.actions.selectModalTab(1, 1, true);
+          _this.props.actions.openAlertDialog("You must be logged in to edit a verse");
+          return;
+        }
+
+        const save = () => {
+          actions.editTargetVerse(chapter, verse, before, _this.state.verseText, _this.state.tags, username);
+          _this.setState({
+            mode: 'default',
+            selections: _this.props.selectionsReducer.selections,
+            verseText: undefined,
+            verseChanged: false,
+            tags: []
+          });
+        };
+        if (_this.state.verseText) {  // if verseText === "" is false
+          save();
+        } else {
+          // alert the user if the text is blank
+          let message = 'You are saving a blank verse. Please confirm.';
+          _this.props.actions.openOptionDialog(message, (option) => {
+            if (option !== "Cancel") save();
+            _this.props.actions.closeAlertDialog();
+          }, "Save Blank Verse", "Cancel");
+        }
+      },
+      validateSelections(verseText) {
+        _this.props.actions.validateSelections(verseText);
+      },
+      toggleReminder() {
+        _this.props.actions.toggleReminder(_this.props.loginReducer.userdata.username);
+      },
+      openAlertDialog(message) {
+        _this.props.actions.openAlertDialog(message);
+      },
+      selectModalTab(tab, section, vis) {
+        _this.props.actions.selectModalTab(tab, section, vis);
+      }
+    };
+  }
+
+  componentWillMount() {
+    let selections = Array.from(this.props.selectionsReducer.selections);
+    this.setState({selections});
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.contextIdReducer.contextId != this.props.contextIdReducer.contextId) {
+      let selections = Array.from(nextProps.selectionsReducer.selections);
+      const {chapter, verse} = nextProps.contextIdReducer.contextId.reference || {};
+      const {targetBible} = nextProps.resourcesReducer.bibles.targetLanguage || {};
+      let verseText = targetBible && targetBible[chapter] ? targetBible[chapter][verse] : "";
+      if (Array.isArray(verseText)) verseText = verseText[0];
+      // normalize whitespace in case selection has contiguous whitespace _this isn't captured
+      verseText = normalizeString(verseText);
+      const mode = nextProps.selectionsReducer.selections.length > 0 || verseText.length === 0 ? 'default' : 'select';
+      this.setState({
+        mode: mode,
+        comments: undefined,
+        verseText: undefined,
+        selections,
+        tags: []
+      });
+    }
+  }
+
+  cancelSelection() {
+    this.actions.changeSelectionsInLocalState(this.props.selectionsReducer.selections);
+    this.actions.changeMode('default');
+  }
+
+  clearSelection() {
+    this.setState({
+      selections: []
+    });
+  }
+
+  saveSelection() {
+    let verseText = this.verseText();
+    // optimize the selections to address potential issues and save
+    let selections = optimizeSelections(verseText, this.state.selections);
+    this.props.actions.changeSelections(selections, this.props.loginReducer.userdata.username);
+    this.actions.changeMode('default');
+  }
+
+  verseText() {
+    const {chapter, verse, bookId} = this.props.contextIdReducer.contextId.reference;
+    const bookAbbr = this.props.projectDetailsReducer.manifest.project.id;
+    const {targetBible} = this.props.resourcesReducer.bibles.targetLanguage;
+    let verseText = "";
+    if (targetBible && targetBible[chapter] && bookId == bookAbbr) {
+      verseText = targetBible && targetBible[chapter] ? targetBible[chapter][verse] : "";
+      if (Array.isArray(verseText)) verseText = verseText[0];
+      // normalize whitespace in case selection has contiguous whitespace _this isn't captured
+      verseText = normalizeString(verseText);
+    }
+    return verseText;
+  }
+
+
+  findIfVerseEdited() {
+    const {contextIdReducer: {contextId}, groupsDataReducer: {groupsData}} = this.props;
+    let result = false;
+
+    if (groupsData[contextId.groupId]) {
+      let groupData = groupsData[contextId.groupId].filter(groupData => {
+        return isEqual(groupData.contextId, contextId);
+      });
+      result = groupData[0].verseEdits;
+    }
+    return result;
+  }
+
+  handleSkip(e) {
+    e.preventDefault();
+    if (this.state.goToNextOrPrevious == "next") {
+      this.actions.skipToNext();
+    } else if (this.state.goToNextOrPrevious == "previous") {
+      this.actions.skipToPrevious();
+    }
   }
 
   componentWillMount() {
@@ -58,21 +334,6 @@ class Container extends React.Component {
     return progress;
   }
 
-
-  getSelections() {
-    contextIdReducer.contextId = groupItemData.contextId;
-    let loadPath = CheckDataLoadActions.generateLoadPath(projectDetailsReducer, contextIdReducer, 'selections');
-    let selectionsObject = CheckDataLoadActions.loadCheckData(loadPath, groupItemData.contextId);
-    let selectionsArray = [];
-
-    if (selectionsObject) {
-      selectionsObject.selections.forEach((selection) => {
-        selectionsArray.push(selection.text);
-      });
-    }
-    let selections = selectionsArray.join(" ");
-  }
-
   getAlignedGLText(currentProjectToolsSelectedGL, contextId, bibles, currentToolName) {
     let alignedGLText = contextId.quote;
     const selectedGL = currentProjectToolsSelectedGL[currentToolName];
@@ -92,7 +353,7 @@ class Container extends React.Component {
     if (translationWords && translationWords[articleId]) {
       currentFile = translationHelps.translationWords[articleId];
     }
-    
+
     let splitLine = currentFile.split('\n');
     if (splitLine.length === 1 && splitLine[0] === "") return "";
     let finalArray = [];
@@ -107,7 +368,7 @@ class Container extends React.Component {
     let splitString = chosenString.split(' ');
     for (let word of splitString) {
       if ((finalString + ' ' + word).length >= maxLength) {
-        finalString+= '...';
+        finalString += '...';
         break;
       }
       finalString += ' ';
@@ -128,25 +389,26 @@ class Container extends React.Component {
       contextIdReducer: {contextId},
       groupsIndexReducer,
       remindersReducer,
-      selectionsReducer:{selections},
+      selectionsReducer: {selections},
       commentsReducer,
       resourcesReducer,
       loginReducer,
       actions,
-    } = this.props;    
+    } = this.props;
 
     if (contextId !== null) {
-      let { translationWords } = resourcesReducer.translationHelps;
+      let {translationWords} = resourcesReducer.translationHelps;
       const languageId = currentProjectToolsSelectedGL[currentToolName];
       const {groupId} = contextId;
       const title = groupsIndexReducer.groupsIndex.filter(item => item.id === groupId)[0].name;
       const glQuote = actions.getGLQuote(languageId, groupId, currentToolName);
       const alignedGLText = this.getAlignedGLText(
         currentProjectToolsSelectedGL, contextId, resourcesReducer.bibles, currentToolName);
-      const checkInfoCardPhrase = this.getCheckInfoCardText(translationWords, contextId.groupId, resourcesReducer.translationHelps)
+      const checkInfoCardPhrase = this.getCheckInfoCardText(translationWords, contextId.groupId, resourcesReducer.translationHelps);
+      const verseText = usfmjs.removeMarker(this.verseText());
 
       return (
-        <div style={{display:'flex', flexDirection:'row'}}>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
           <GroupMenu
             translate={translate}
             getSelections={(contextId) => actions.getSelectionsFromContextId(contextId, projectSaveLocation)}
@@ -159,14 +421,14 @@ class Container extends React.Component {
             contextIdReducer={{contextId}}
             projectDetailsReducer={{manifest, projectSaveLocation}}
             actions={actions} />
-          <div style={{display:'flex', flexDirection:'column', width:'100%'}}>
+          <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
             <CheckInfoCard
               title={title}
               phrase={checkInfoCardPhrase}
               seeMoreLabel={translate('see_more')}
               showSeeMoreButton={this.state.showHelps}
               onSeeMoreClick={this.toggleHelps.bind(this)} />
-            <VerseCheck 
+            <VerseCheck
               alignedGLText={alignedGLText}
               projectDetailsReducer={{currentProjectToolsSelectedGL, manifest, projectSaveLocation}}
               loginReducer={loginReducer}
@@ -178,7 +440,20 @@ class Container extends React.Component {
               toolsReducer={toolsReducer}
               groupsDataReducer={groupsDataReducer}
               remindersReducer={remindersReducer}
-              actions={actions} />
+              actions={this.actions}
+              verseText={verseText}
+              mode={this.state.mode}
+              dialogModalVisibility={this.state.dialogModalVisibility}
+              commentChanged={this.state.commentChanged}
+              findIfVerseEdited={this.findIfVerseEdited}
+              tags={this.state.tags}
+              verseChanged={this.state.verseChanged}
+              selections={this.state.selections}
+              saveSelection={this.saveSelection}
+              cancelSelection={this.cancelSelection}
+              clearSelection={this.clearSelection}
+              handleSkip={this.handleSkip}
+            />
           </div>
         </div>
       );
