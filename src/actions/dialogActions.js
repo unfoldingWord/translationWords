8555e14f7d9ac13e8cf388f24cb8491c4364ce16
Bruce McLean <mcleanb@photo-nomad.net>
2018-05-30 09:08:35 -0400

    Merge branch 'feature-jay-4318' of https://github.com/translationCoreApps/translationWords_Check_plugin into feature-jay-4318
    # Conflicts:
    #	src/Container.js

--- src/actions/dialogActions.js
+++
@@ -1,7 +0,0 @@
-export function handleOpenDialog(goToNextOrPrevious) {
-  _this.setState({goToNextOrPrevious});
-  _this.setState({dialogModalVisibility: true});
-}
-export function handleCloseDialog() {
-  _this.setState({dialogModalVisibility: false});
-}
