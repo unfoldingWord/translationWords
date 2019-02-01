28a2448e263c144e74d5e89bded9251e7e7b935f
RoyalSix <jay.a.scottjr@biola.edu>
2019-02-01 09:02:34 -0500

    initial implementation

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,8 @@
 import {ToolApi} from 'tc-tool';
 import path from 'path-extra';
+import isEqual from 'deep-equal';
+import usfm from "usfm-js";
+import {checkSelectionOccurrences} from 'selections';
 
 export default class Api extends ToolApi {
 
@@ -11,13 +14,136 @@ export default class Api extends ToolApi {
     this._loadBookSelections = this._loadBookSelections.bind(this);
     this._loadVerseSelections = this._loadVerseSelections.bind(this);
     this._loadCheckData = this._loadCheckData.bind(this);
+    this.toolName = 'translationWords';
   }
 
   /**
    * Lifecycle method
    */
   toolWillConnect() {
-    // TODO: implement
+    this.validateBookSelections(this.props);
+  }
+
+  validateBookSelections() {
+    const {
+      tc: {
+        targetBook
+      }
+    } = this.props;
+    // iterate through target chapters and validate selections
+    const results = {selectionsChanged: false};
+    for (const chapter of Object.keys(targetBook)) {
+      if (isNaN(chapter) || parseInt(chapter) === -1) continue;
+      this.validateChapterSelections(chapter, results);
+    }
+    if (results.selectionsChanged) {
+      this._showResetDialog();
+    }
+  }
+
+  _showResetDialog() {
+    const {
+      tool: {
+        translate
+      }
+    } = this.props;
+    this.props.tc.showIgnorableAlert('selections_invalidated', translate('selections_invalidated'));
+  }
+
+  /**
+   * verifies all the selections for chapter to make sure they are still valid.
+   * This expects the book resources to have already been loaded.
+   * Books are loaded when a project is selected.
+   * @param {String} chapter
+   * @param {Object} results - for keeping track if any selections have been reset.
+   */
+  validateChapterSelections(chapter, results) {
+    const {
+      tc: {
+        targetBook,
+        contextId: {reference: {bookId}},
+      }
+    } = this.props;
+    let changed = results.selectionsChanged; // save initial state
+    if (targetBook[chapter]) {
+      const bibleChapter = targetBook[chapter];
+      if (bibleChapter) {
+        for (let verse of Object.keys(bibleChapter)) {
+          const verseText = bibleChapter[verse];
+          const contextId = {
+            reference: {
+              bookId,
+              chapter: parseInt(chapter),
+              verse: parseInt(verse)
+            }
+          };
+          results.selectionsChanged = false;
+          this.validateAllSelectionsForVerse(verseText, results, false, contextId);
+          changed = changed || results.selectionsChanged;
+        }
+      }
+    }
+    results.selectionsChanged = changed;
+  }
+
+
+  /**
+ * verify all selections for current verse
+ * @param {string} targetVerse - new text for verse
+ * @param {object} results - keeps state of
+ * @param {Boolean} skipCurrent - if true, then skip over validation of current contextId
+ * @param {Object} contextId - optional contextId to use, otherwise will use current
+ * @param {Boolean} warnOnError - if true, then will show message on selection change
+ * @return {Function}
+ */
+  validateAllSelectionsForVerse(targetVerse, results, skipCurrent = false, contextId = null, warnOnError = false) {
+    let {
+      tc: {
+        username,
+        changeSelections
+      }
+    } = this.props;
+    const initialSelectionsChanged = results.selectionsChanged;
+    const groupsDataForVerse = this.getGroupDataForVerse(contextId);
+    let filtered = null;
+    results.selectionsChanged = false;
+
+    for (let groupItemKey of Object.keys(groupsDataForVerse)) {
+      const groupItem = groupsDataForVerse[groupItemKey];
+      for (let checkingOccurrence of groupItem) {
+        const selections = checkingOccurrence.selections;
+        if (!skipCurrent || !this.sameContext(contextId, checkingOccurrence.contextId)) {
+          if (selections && selections.length) {
+            if (!filtered) {  // for performance, we filter the verse only once and only if there is a selection
+              filtered = usfm.removeMarker(targetVerse); // remove USFM markers
+            }
+            const validSelections = checkSelectionOccurrences(filtered, selections);
+            if (selections.length !== validSelections.length) {
+              results.selectionsChanged = true;
+              changeSelections([], username, true, checkingOccurrence.contextId); // clear selection
+            }
+          }
+        }
+      }
+    }
+
+    if (warnOnError && (initialSelectionsChanged || results.selectionsChanged)) {
+      this._showResetDialog();
+    }
+  }
+
+  /**
+   * returns true if contextIds are a match for reference and group
+   * @param {Object} contextId1
+   * @param {Object} contextId2
+   * @return {boolean}
+   */
+  sameContext(contextId1, contextId2) {
+    if (!!contextId1 && !!contextId2) {
+      return isEqual(contextId1.reference, contextId2.reference) &&
+        (contextId1.groupId === contextId2.groupId);
+    }
+    return false;
   }
 
   /**
@@ -270,4 +396,38 @@ export default class Api extends ToolApi {
     return selections;
   }
 
+  /**
+ * @description gets the group data for the current verse from groupsDataReducer
+ * @param {Object} state
+ * @param {Object} contextId
+ * @return {object} group data object.
+ */
+  getGroupDataForVerse(contextId) {
+    const {
+      tc: {
+        project: {
+          getGroupsData
+        }
+      }} = this.props;
+    const groupsData = getGroupsData(this.toolName);
+    const filteredGroupData = {};
+    for (let groupItemKey of Object.keys(groupsData)) {
+      const groupItem = groupsData[groupItemKey];
+      if (groupItem) {
+        for (let check of groupItem) {
+          try {
+            if (isEqual(check.contextId.reference, contextId.reference)) {
+              if (!filteredGroupData[groupItemKey]) {
+                filteredGroupData[groupItemKey] = [];
+              }
+              filteredGroupData[groupItemKey].push(check);
+            }
+          } catch (e) {
+            console.warn(`Corrupt check found in group "${groupItemKey}"`, check);
+          }
+        }
+      }
+    }
+    return filteredGroupData;
+  }
 }
